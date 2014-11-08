2bf1c76d617aa10003833040699ad8afedd675ca
Nick A Breslin <nickbreslin@gmail.com>
2014-11-08 15:59:52 -0800

    Fieldmap

---
+++ includes/class-fieldmap.php
@@ -0,0 +1,14 @@
+/*
+external_id, enum
+title, string
+author, string
+tags, string
+body, text
+excerpt, text
+category, enum
+url, enum
+image, enum
+timestamp, date
+object_type, enum
+updated_at, date
+*/
